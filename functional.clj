f31ba73cf2dc2c8e0d4284b54ab0c1387dbd1901
Kevin Downey <redchin@gmail.com>
2009-02-19 13:22:02 -0800

    split transform into transformr and transforml, so can transform from the left or from right, switch $ to transformr, previoulsy a $ b $ c became ((a b) c) now  (a (b c))

--- functional.clj
+++ functional.clj
@@ -10,13 +10,24 @@
           (set? x)    (set a)
           (seq? x)    (seq a))))
 
-(defn transform [se pred fn]
-      (loop [se (zip/seq-zip se)]
-            (if (zip/end? se)
-              (zip/root se)
+(defn transform [se pred fn adv fin]
+      (loop [se se]
+            (if (fin se)
+              se
               (if (pred se)
-                (recur (zip/next (fn se)))
-                (recur (zip/next se))))))
+                (recur (adv (fn se)))
+                (recur (adv se))))))
+
+(defn transforml [se pred fn]
+      (transform se pred fn zip/next zip/end?))
+
+(defn transformr [se pred fn]
+      (transform se pred fn zip/prev (comp not zip/prev)))
+
+(defn fast-fwd [se]
+      (if (zip/end? (zip/next se))
+        se
+        (recur (zip/next se))))
 
 (defn uncurry [x]
       (fn uc [& y]
@@ -31,23 +42,29 @@
 (defn call [x & y] (apply x y))
 
 (defmacro pl [& forms]
-  (let [x (transform forms
-                     #(= "$" (let [c (zip/node %)] (and (symbol? c) (name c))))
-                     #(let [n (list (zip/node (zip/left %)) (zip/node (zip/right %)))]
-                        (-> % zip/left (zip/insert-left n)
-                            zip/remove zip/next zip/remove zip/next zip/remove)))
-        x (transform x
+  (let [x (zip/seq-zip forms)
+        x (transformr (fast-fwd x)
+                      #(= "$" (and (symbol? (zip/node %)) (name (zip/node %))))
+                      #(let [n (list (zip/node (zip/left %)) (zip/node (zip/right %)))]
+                         (-> % zip/remove (zip/insert-left n) zip/right zip/remove zip/remove)))
+        x (transforml x
                      #(= "·" (let [c (zip/node %)] (and (symbol? c) (name c))))
                      #(let [n (list 'comp (zip/node (zip/left %)) (zip/node (zip/right %)))]
                         (-> % zip/left (zip/insert-left n)
                             zip/remove zip/next zip/remove zip/next zip/remove)))
-        x (transform x
+        x (zip/seq-zip (zip/root x))
+        x (transforml x
                      #(= 9021 (and (symbol? (zip/node %)) (.codePointAt (name (zip/node %)) 0)))
                      #(-> % (zip/replace (list 'uncurry (symbol (subs (name (zip/node %)) 1))))))
-        x (transform x
+        x (zip/seq-zip (zip/root x))
+        x (transforml x
                      #(= 8597 (and (symbol? (zip/node %)) (.codePointAt (name (zip/node %)) 0)))
-                     #(-> % (zip/replace (list 'flip (symbol (subs (name (zip/node %)) 1))))))]
+                     #(-> % (zip/replace (list 'flip (symbol (subs (name (zip/node %)) 1))))))
+        x (zip/root x)]
     `(do ~@x)))
 
-;; (pl (↕map (replicate 3 (↕apply (vector (↕map (range 10) inc · inc · inc)) call · ⌽* $ 10 · call · (⌽+ -2) map)) shuffle))
-;; (pl (map call · ⌽+ $ 5 range $ 5))
+(pl
+  (↕map (replicate 3 (↕apply vector $ (↕map range $ 10 inc · inc · inc) call · ⌽* $ 10 · call · (⌽+ -2) map)) shuffle))
+ 
+;; (pl
+;;   (↕map (↕map (replicate 3 (range 3)) call · (⌽map inc)) shuffle))
