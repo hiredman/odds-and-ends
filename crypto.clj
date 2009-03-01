20d510e27a82967141731188e03e84eb9d0c3b3f
Kevin Downey <redchin@gmail.com>
2009-02-28 19:33:26 -0800

    weird whitespacing

--- crypto.clj
+++ crypto.clj
@@ -32,45 +32,45 @@
 (defn write-key
       "write key to file"
       [key file]
-	  (with-open [f (-> file java.io.File. java.io.FileOutputStream.)]
-				 (.write f (.getEncoded key))))
+      (with-open [f (-> file java.io.File. java.io.FileOutputStream.)]
+                 (.write f (.getEncoded key))))
 
 (defn read-key
       "read key from file"
       [file]
-	  (with-open [f (-> file java.io.File. java.io.FileInputStream.)]
-				 (let [buf (make-array Byte/TYPE (.available f))]
-				   (.read f buf)
-				   (SecretKeySpec. buf "DES"))))
+      (with-open [f (-> file java.io.File. java.io.FileInputStream.)]
+                 (let [buf (make-array Byte/TYPE (.available f))]
+                   (.read f buf)
+                   (SecretKeySpec. buf "DES"))))
 
 (defn cipher-output-stream [key stream]
-	  (let [c (doto (cipher) (.init Cipher/ENCRYPT_MODE key))]
-		(-> stream (CipherOutputStream. c))))
+      (let [c (doto (cipher) (.init Cipher/ENCRYPT_MODE key))]
+        (-> stream (CipherOutputStream. c))))
 
 (defn cipher-input-stream [key stream]
-	  (let [c (doto (cipher) (.init Cipher/DECRYPT_MODE key))]
-		(-> stream (CipherInputStream. c))))
+      (let [c (doto (cipher) (.init Cipher/DECRYPT_MODE key))]
+        (-> stream (CipherInputStream. c))))
 
 (defn cipher-writer [key file]
-	  (let [op-s (-> file File. FileOutputStream.)
-			cop-s (cipher-output-stream key op-s)]
-		(-> cop-s OutputStreamWriter.)))
+      (let [op-s (-> file File. FileOutputStream.)
+            cop-s (cipher-output-stream key op-s)]
+        (-> cop-s OutputStreamWriter.)))
 
 (defn cipher-reader [key file]
-	  (let [op-s (-> file File. FileInputStream.)
-			cop-s (cipher-input-stream key op-s)]
-		(-> cop-s InputStreamReader.)))
+      (let [op-s (-> file File. FileInputStream.)
+            cop-s (cipher-input-stream key op-s)]
+        (-> cop-s InputStreamReader.)))
 
 (let [k (generate-key) c (cipher)
-      text  (apply str (map (comp char (partial + 40)) (take 100 (repeatedly #(rand-int 80)))))
-      cleartext (.getBytes text)
-      ciphertext (.doFinal (doto c (.init Cipher/ENCRYPT_MODE k)) cleartext)
-      cleartext2 (.doFinal (doto c (.init Cipher/DECRYPT_MODE k)) ciphertext)
-      file (File/createTempFile "crypto" "test") _ (.deleteOnExit file)
-      file (.toString file) _ (write-key k file)
-	  k2 (read-key file)]
+     text  (apply str (map (comp char (partial + 40)) (take 100 (repeatedly #(rand-int 80)))))
+     cleartext (.getBytes text)
+     ciphertext (.doFinal (doto c (.init Cipher/ENCRYPT_MODE k)) cleartext)
+     cleartext2 (.doFinal (doto c (.init Cipher/DECRYPT_MODE k)) ciphertext)
+     file (File/createTempFile "crypto" "test") _ (.deleteOnExit file)
+     file (.toString file) _ (write-key k file)
+     k2 (read-key file)]
   (assert (= (seq cleartext) (seq cleartext2)))
   (assert (= k k2))
   (with-open [o (cipher-writer k file)]
-			 (.write o text))
+             (.write o text))
   (assert (= text (with-open [i (java.io.BufferedReader. (cipher-reader k file))] (.readLine i)))))
