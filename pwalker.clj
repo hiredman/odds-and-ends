47e32cbfe21957dc538722ed4f62b77899735954
Kevin Downey <redchin@gmail.com>
2009-03-08 18:35:46 -0700

    docstrings, walk now returns a ref

--- pwalker.clj
+++ pwalker.clj
@@ -12,11 +12,14 @@
 (defn- execute [#^Callable fn]
        (.execute #^ThreadPoolExecutor runner fn))
 
-(defn walk [dir dir-fn file-fn r]
+(defn walk
+      "takes a direcotyr as a File, a function to be applied to all directories
+      a fn to be applied to all files, returns a ref for bookkeeping"
+      ([dir dir-fn file-fn]
+       (walk dir dir-fn file-fn (ref 0)))
+      ([dir dir-fn file-fn r]
        (let [files (.listFiles #^File dir)
-             [++1 +-1] (if r
-                         [#(dosync (alter r inc)) #(dosync (alter r dec))]
-                         (repeat 2 #(do nil)))]
+             [++1 +-1] [#(dosync (alter r inc)) #(dosync (alter r dec))]]
          (doseq [entry files]
                 (++1)
                 (if (.isDirectory entry)
@@ -26,9 +29,15 @@
                               (+-1)))
                   (do
                     (file-fn entry)
-                    (+-1))))))
+                     (+-1)))))
+       r))
 
-(defn collect-files [dir & opt]
+
+
+(defn collect-files
+      "takes a directory name as a string, and optionally a predicate to filter files
+      and walks a directory in a recursive and concurrent manner"
+      [dir & opt]
       (let [q (LinkedBlockingQueue.)
             f (fn f [#^LinkedBlockingQueue q]
                   (lazy-seq
@@ -36,7 +45,6 @@
                       (cons (.take q) (f q)))))
             fil (if opt (first opt) identity) 
             x #(when (fil %) (.put q %))
-            count (ref 0)]
-        (walk (File. #^String dir) identity x count)
+            count (walk (File. #^String dir) identity x)]
         (take-while #(when (or (not= 0 @count) %) %)
                     (repeatedly #(.poll q)))))
