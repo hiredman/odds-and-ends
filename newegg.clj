0ee31f81f431c092995e04bc755c9fb682843fea
Kevin Downey <redchin@gmail.com>
2009-12-04 12:23:00 -0800

    working wishlist price query

---
+++ newegg.clj
@@ -0,0 +1,30 @@
+;requires tagsoup
+(defn startparse-tagsoup [s ch]
+      (let [p (org.ccil.cowan.tagsoup.Parser.)]
+                    (.setContentHandler p ch)
+                    (.parse p s)))
+
+(defn zip-soup [url]
+      (clojure.zip/xml-zip (clojure.xml/parse url startparse-tagsoup)))
+
+
+(def wishlist-url "http://secure.newegg.com/WishList/PublicWishDetail.aspx?WishListNumber")
+
+(defn wishlist [num]
+  (format "%s=%s" wishlist-url (str num)))
+
+(defn pick [choices tag]
+  ((comp first filter) #(= tag (:tag %)) choices))
+
+(def wishlist-price
+  (comp #(BigDecimal. %)
+        (partial into-array Character/TYPE)
+        rest first :content last
+        (partial filter #(= "totalPrice" (-> % :attrs :class)))
+        (partial filter #(= :td (:tag %)))
+        (partial tree-seq map? (comp seq :content))
+        first
+        zip-soup
+        wishlist))
+
+(wishlist-price 8795214)
